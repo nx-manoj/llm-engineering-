65073874df1f8f3ca3cbfe11cd54e64e0059c7f4
nx-manoj <manojyeduvaka@gmail.com>
2026-06-22 16:18:25 +0530

    first commit

---
+++ Project-1 (website summarizer)/scrap.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+
+def fetch_website_contents(url):
+    response = requests.get(url,headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    title = soup.title.string if soup.title else 'No title found'
+
+    if soup.body:
+        for irrelevant in soup.body(['script','style','image','input']):
+            irrelevant.decompose()
+
+        text = soup.body.get_text(separator='\n',strip=True)
+    else:
+        text = ""
+    
+    return (title + '\n\n' + text)[:2000]
